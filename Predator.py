b75d07c82decd674563c6783bcdf6a17c9ce7f72
Craig Reynolds <cwr@red3d.com>
2022-09-22 12:10:32 -0700

    Lower starve threshold, must miss more meals.
    Hoping to lower frequency of starvation, hence randomize a model, hence depress in_disk metric.
    Add Predator.success_history_ratio, change from 0.33 to 0.2.
    Cleaned out moved/commented-out code in EvoCamoVsLearnPredPop.py
    General clean up in Predator.py and FineTuningDataset.py.

--- Predator.py
+++ Predator.py
@@ -16,34 +16,28 @@ import random
 import numpy as np
 import DiskFind as df
 import tensorflow as tf
-################################################################################
-# TODO 20220921 re-refactor FineTuningDataset - why bother with a class?
-#from FineTuningDataset import FineTuningDataset
 import FineTuningDataset as ftd
-################################################################################
 
 class Predator:
     """Represents a Predator in the camouflage simulation. It has a CNN-based
-       model of visual hunting that identified the position of likely prey."""
+       model of visual hunting that identifies the position of likely prey."""
 
     # Global list of active Predators (as a class variable).
     population = []
-    
-    ############################################################################
-    # TODO 20220919 for modularity, pass model to initialize_predator_population
-    
-#    # Cache default_pre_trained_model (as a class variable).
-#    default_pre_trained_model = read_default_pre_trained_model()
 
-    # Cache default_pre_trained_model (as a class variable).
+    # Cache the standard default_pre_trained_model (as a class variable).
     default_pre_trained_model = None
-
-    ############################################################################
     
     # How much recent predation success data is kept,
     # and how much of it must be non-zero to avoid starvation.
     success_history_max_length = 20
-    success_history_min_meals = success_history_max_length * 0.33
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # TODO 20220922 lower starvation threshold, must miss more meals: .33 to .2
+#    success_history_min_meals = success_history_max_length * 0.33
+#    success_history_min_meals = success_history_max_length * 0.2
+    success_history_ratio = 0.2
+    success_history_min_meals = success_history_max_length*success_history_ratio
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
     # Keep track of how often selected prey is nearest center:
     nearest_center = 0
@@ -83,42 +77,26 @@ class Predator:
               '(nearest_center =', nc, ', steps =', self.step + 1, ')')
 
         # Convert training data list to np arrays
-#        images_array = np.array(fine_tuning_dataset.fine_tune_images)
-#        labels_array = np.array([x[0] for x in fine_tuning_dataset.fine_tune_labels])
-        ################################################################################
-        # TODO 20220921 re-refactor FineTuningDataset - why bother with a class?
-#        images_array = np.array(FineTuningDataset().fine_tune_images)
-#        labels_array = np.array([x[0] for x in FineTuningDataset().fine_tune_labels])
         images_array = np.array(ftd.fine_tune_images)
         labels_array = np.array([x[0] for x in ftd.fine_tune_labels])
-        ################################################################################
-
-        # print('images_array.shape =', images_array.shape,
-        #       '-- labels_array.shape =', labels_array.shape)
 
         # Skip fine-tuning until dataset is large enough (10% of max size).
-#        if images_array.shape[0] > (fine_tuning_dataset.max_training_set_size * 0.1):
-        ################################################################################
-        # TODO 20220921 re-refactor FineTuningDataset - why bother with a class?
-#        if images_array.shape[0] > (FineTuningDataset().max_training_set_size * 0.1):
         if images_array.shape[0] > (ftd.max_training_set_size * 0.1):
-        ################################################################################
             # TODO 20220823 -- run fine-tuning on CPU only.
             print('Running on CPU ONLY!')
             with tf.device('/cpu:0'):
                 # Do fine-tuning training step using data accumulated during run.
                 history = self.model.fit(x=images_array, y=labels_array)
+            ####################################################################
+            # TODO 20220921 turn off temporarily. Redesign. PredatorServer module?
             # Keep log of in_disk metric:
-            write_in_disk_log(self.step, history)
-
-        # print(end='  ')
-        # self.print_model_trace()
+#            write_in_disk_log(self.step, history)
+            ####################################################################
         
         # Keep recent win/loss record for this predator for starvation pruning.
         self.record_predation_success(prediction, prey_centers_xy3)
 
     # Copy the neural net model of a given predator into this one.
-    
     def copy_model_of_another_predator(self, another_predator):
         self.copy_model(another_predator.model)
 
@@ -150,21 +128,6 @@ class Predator:
                 print(round(value, 2), end = " ")
         print()
 
-    ############################################################################
-    # TODO 20220919 for modularity, pass model to initialize_predator_population
-
-#    # Create the given number of Predator objects
-#    # (TODO maybe the pretrained model should be an arg?)
-#    def initialize_predator_population(population_size):
-#        for i in range(population_size):
-#            p = Predator()
-#            print('Predator instance address:', id(p))
-#            # TODO 20220907 maybe just do this by default in constructor?
-#            p.copy_model(Predator.default_pre_trained_model)
-#            p.jiggle_model()
-#            p.print_model_trace()
-#        print('Created population of', population_size, 'Predators.')
-
     # Create the given number of Predator objects
     # (TODO maybe the pretrained model should be an arg?)
     def initialize_predator_population(population_size, pre_trained_model):
@@ -180,8 +143,6 @@ class Predator:
             p.print_model_trace()
         print('Created population of', population_size, 'Predators.')
 
-    ############################################################################
-
     # Randomly select "size" unique Predators from population for a Tournament.
     def choose_for_tournament(size = 3):
         assert len(Predator.population) >= size, "Population smaller than tournament size."
@@ -190,7 +151,6 @@ class Predator:
     # Maintain record of recent hunts and which ones were successfu.
     def record_predation_success(self, prediction_xy, prey_centers_xy3):
         radius = df.relative_disk_radius()
-#        distance = aim_error(prediction_xy, prey_centers_xy3)
         distance = df.aim_error(prediction_xy, prey_centers_xy3)
         # Append a 0 (fail) or 1 (succeed) to history.
         self.successes.append(0 if distance < radius else 1)
@@ -230,4 +190,3 @@ def weight_perturbation(model, max_range):
                                                max_range / 2,
                                                dtype=tf.float32)
             weight.assign_add(random_weights)
-
